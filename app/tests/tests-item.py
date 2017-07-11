c78bc2bce4e2b9709003d8a4df2df5fff24cf2fd
Brian Serumaga <bsk20111@hotmail.com>
2017-07-11 23:03:27 +0300

    Added tests for item and added functionality

---
+++ app/tests/tests-item.py
@@ -0,0 +1,13 @@
+from classes.item import Item
+import unittest
+
+
+class ItemTestCases(unittest.TestCase):
+
+    def setUp(self):
+        name = "Go to Bahamas"
+        description = "To go to the Bahamas to see what's up."
+        self.item1 = Item(name, description)
+
+    def test_for_default_progress(self):
+        self.assertFalse(self.item1.progress, msg="The default progress is wrong.")
