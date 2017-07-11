07af437f07236492b386c79211bda02487478c70
Brian Serumaga <bsk20111@hotmail.com>
2017-07-11 23:06:50 +0300

    Add tests for bucket list class and functionality to pass the tests

---
+++ app/classes/bucket_list.py
@@ -0,0 +1,25 @@
+from classes.item import Item
+
+
+class BucketList(object):
+    item_list = []
+
+    def __init__(self, name):
+        self.name = name
+
+    def add_bucket_list_item(self, item_name, description):
+        item1 = Item(item_name, description)
+        self.item_list.append(item1)
+
+    def view_bucket_list_item(self, name):
+        for obj in self.item_list:
+            if obj.name == name:
+                return obj
+
+    def edit_bucket_list_item(self):
+        pass
+
+    def delete_bucket_list_item(self, name):
+        for obj in self.item_list:
+            if obj.name == name:
+                self.item_list.remove(obj)
---
+++ app/tests/tests_bucket_list.py
@@ -0,0 +1,34 @@
+from classes.bucket_list import BucketList
+import unittest
+
+
+class BucketListTestCase(unittest.TestCase):
+
+    def setUp(self):
+        self.exampleBucketList = BucketList('Africa')
+
+    def test_BucketList_class(self):
+        self.assertEqual(type(self.exampleBucketList.name), str,
+                         msg="The class doesn't exist")
+
+    def test_for_adding_item(self):
+        name= 'Tokyo'
+        description = "Am going to Tokyo this year."
+        self.exampleBucketList.add_bucket_list_item(name, description)
+        self.assertEqual(len(self.exampleBucketList.item_list), 1,
+                         msg="The item is not being added properly")
+
+    def test_for_viewing_item(self):
+        example = self.exampleBucketList.view_bucket_list_item("Tokyo")
+        self.assertEqual(example.description, "Am going to Tokyo this year.",
+                         msg="Returning the wrong result")
+
+    def test_for_deleting_item(self):
+        name = 'China'
+        description = "Am going to China this year."
+        self.exampleBucketList.add_bucket_list_item(name, description)
+        self.exampleBucketList.delete_bucket_list_item("China")
+        self.assertEqual(len(self.exampleBucketList.item_list), 1,
+                         msg="The item was not deleted")
+
+
