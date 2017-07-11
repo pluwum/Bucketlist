e14618893b270599e7646978bc53e1d509b3e009
Brian Serumaga <bsk20111@hotmail.com>
2017-07-11 23:00:41 +0300

    Edit the folder structure

--- app/form.py
+++
@@ -1,8 +0,0 @@
-"""from flask.ext.wtf import Form
-from wtforms.fields import TextField, BooleanField
-from wtforms.validators import Required
-
-class LoginForm(Form):
-    openid = StringField('openid', validators=[DataRequired()])
-    remember_me = BooleanField('remember_me', default=False)
-"""
