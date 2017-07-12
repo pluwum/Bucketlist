d04603a5ed96e0534c655d7e887f64fa09617c51
Brian Serumaga <bsk20111@hotmail.com>
2017-07-12 12:19:30 +0300

    Add the test and functionality of the edit bucket list method

--- app/bucketlist/user.py
+++ app/bucketlist/user.py
@@ -1,18 +1,34 @@
+"""Module containing the User class"""
 from ..bucketlist.bucket_list import BucketList
 
 
 class User(object):
-    bucket_lists = []
+    """Class containing the user methods"""
 
     def __init__(self, fname, sname, email, password):
         self.fname = fname
         self.sname = sname
         self.email = email
         self.password = password
+        self.bucket_lists = []
 
-    def add_new_bucket_list(self,bucket_list_name):
+    def add_new_bucket_list(self, bucket_list_name):
+        """Method for adding new bucketlist"""
         new_bucket_list = BucketList(bucket_list_name)
         self.bucket_lists.append(new_bucket_list)
 
+    def view_bucket_list(self, bucket_list_name):
+        """Method for viewing  the bucket list"""
+        for obj in self.bucket_lists:
+            if obj.name == bucket_list_name:
+                return obj
+
+    def edit_bucket_list(self, bucket_list_name, new_bucket_list_name, new_progress = False):
+        """Method for editing the bucket list"""
+        for obj in self.bucket_lists:
+            if obj.name == bucket_list_name:
+                obj.name = new_bucket_list_name
+                obj.progress = new_progress
+                return obj
 
 
--- app/tests/tests_user.py
+++ app/tests/tests_user.py
@@ -1,16 +1,26 @@
+"""Module containing the User Class tests"""
 from ..bucketlist.user import User
 import unittest
 
 
 class UserTestCase(unittest.TestCase):
+    """Class containing the user TestCases"""
     def setUp(self):
         self.example_user = User('Brian', 'Serumaga', 'bsk20111@hotmail.com', 'pumpkin')
+        self.example_user.add_new_bucket_list("Programming")
 
     def test_for_adding_bucketlist(self):
+        """Test for adding a new bucket list"""
         self.example_user.add_new_bucket_list("Growth")
-        self.assertEqual(len(self.example_user.bucket_lists), 1,
+        self.assertEqual(len(self.example_user.bucket_lists), 2,
                          msg="New Bucket list has been added.")
 
-    #def test_view_bucketlist(self):
-    #    example1 = self.example_user.view_bucket_list('Growth')
-    #    self.assertEqual(example1.name, 'Growth', msg="Unable to view the bucket list")
+    def test_view_bucketlist(self):
+        """Test for viewing a bucket list"""
+        example1 = self.example_user.view_bucket_list('Programming')
+        self.assertEqual(example1.name, 'Programming', msg="Unable to view the bucket list")
+
+    def test_editing_bucket_list(self):
+        """Test for editing a bucket list"""
+        result = self.example_user.edit_bucket_list('Programming', 'Agriculture')
+        self.assertEqual(result.name, 'Agriculture', msg="The bucket list was not edited")
