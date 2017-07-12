c77a7bbf659f257171d35cdfa8e5fbb1d1b67dff
Brian Serumaga <bsk20111@hotmail.com>
2017-07-12 14:25:12 +0300

    Edit the code to meet PEP8 conventions

--- app/bucketlist/bucket_list_app.py
+++ app/bucketlist/bucket_list_app.py
@@ -3,9 +3,23 @@ from ..bucketlist.user import User
 
 
 class BucketListApp(object):
+    """Class containing the bucket list methods"""
     def __init__(self):
         self.current_user = None
         self.list_of_users = []
 
-    def add_user(self):
-        pass
+    def add_user(self, fname, lname, email, password):
+        """Method for adding a user"""
+        new_user = User(fname, lname, email, password)
+        self.list_of_users.append(new_user)
+
+    def login_user(self, email, password):
+        """Method for user login"""
+        for obj in self.list_of_users:
+            if obj.email == email and obj.password == password:
+                self.current_user = obj
+            return 'Invalid Login Details'
+
+    def sign_out(self):
+        """Method for  signing out a user"""
+        self.current_user = None
--- app/tests/tests_bucket_list_app.py
+++ app/tests/tests_bucket_list_app.py
@@ -1,7 +1,32 @@
+"""Module containing the TestCases for the Bucket List Class"""
 import unittest
 from ..bucketlist.bucket_list_app import BucketListApp
 
 
 class BucketListAppTestcases(unittest.TestCase):
+    """Class containing the bucket list app TestCases"""
     def setUp(self):
         self.example1 = BucketListApp()
+        self.example1.add_user('Nelson', 'Kiggundu', 'Nking@hotmail.com', '123456')
+
+    def test_for_adding_users(self):
+        """Test for adding a user functionality"""
+        self.example1.add_user('Brian', 'Serumaga', 'bsk2012@hotmail.com', '12345')
+        self.assertEqual(len(self.example1.list_of_users), 2, msg="The user wasn't added")
+
+    def test_for_login_user(self):
+        """Test for login in user function"""
+        self.example1.login_user('Nking@hotmail.com', '123456')
+        self.assertEqual(self.example1.current_user.email, 'Nking@hotmail.com',
+                         msg="The user was unable to login.")
+
+    def test_for_invalid_login_user(self):
+        """Test for login in user function"""
+        self.assertEqual(self.example1.login_user('Nking@hotmail.com', '123'),
+                         'Invalid Login Details', msg="The user was unable to login.")
+
+    def test_for_user_sign_out(self):
+        """Test for when user signs out"""
+        self.example1.sign_out()
+        self.assertIsNone(self.example1.current_user,
+                          msg=" The person wasn't logged out successfully")
