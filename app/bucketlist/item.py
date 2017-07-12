18c732412c0f702478031ed1a99aa5341047a280
Brian Serumaga <bsk20111@hotmail.com>
2017-07-12 10:26:07 +0300

    Edited item class and item tests to meet PEP8 conventions

--- app/bucketlist/item.py
+++ app/bucketlist/item.py
@@ -1,6 +1,9 @@
+"""Module containing item class"""
+
+
 class Item(object):
+    """Class used to store bucket list item properties"""
     def __init__(self, name, description):
         self.name = name
         self.description = description
         self.progress = False
-
--- app/tests/tests_item.py
+++ app/tests/tests_item.py
@@ -1,13 +1,15 @@
+"""Module containing the TestCase for Item Class"""
 import unittest
 from ..bucketlist.item import Item
 
 
 class ItemTestCases(unittest.TestCase):
-
+    """Class for the item TestCases"""
     def setUp(self):
         name = "Go to Bahamas"
         description = "To go to the Bahamas to see what's up."
         self.item1 = Item(name, description)
 
     def test_for_default_progress(self):
+        """Test to check for the default progress"""
         self.assertFalse(self.item1.progress, msg="The default progress is wrong.")
