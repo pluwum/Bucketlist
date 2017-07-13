13af485d94f8da520f35d08472e37d246f49e316
Brian Serumaga <bsk20111@hotmail.com>
2017-07-13 15:03:47 +0300

    Added the create item route

--- app/views.py
+++ app/views.py
@@ -1,9 +1,11 @@
-from flask import render_template, redirect, request, url_for, session
+from flask import render_template, redirect, request, url_for
 from app import app
 from .bucketlist.bucket_list_app import BucketListApp
+from .bucketlist.bucket_list import BucketList
+from .bucketlist.bucket_list import Item
 
 start_app = BucketListApp()
-app.secret_key = 'F12Zr47j3yX R~X@H!jmM]Lwf/,?KT'
+
 
 
 @app.route('/')
@@ -37,8 +39,11 @@ def register():
 
 @app.route('/home', methods=['GET'])
 def home():
-    if start_app.current_user is None:
+    if not start_app.current_user:
         return redirect(url_for('login'))
+    elif len(start_app.current_user.bucket_lists) > 0:
+        return render_template('home.html',
+                               bucket_lists=start_app.current_user.bucket_lists)
     return render_template('home.html')
 
 
@@ -50,8 +55,41 @@ def sign_out():
     return redirect(url_for('login'))
 
 
-@app.route('/create_bucketlist', methods=['GET'])
+@app.route('/create_bucketlist', methods=['GET', 'POST'])
 def create_bucketlist():
     if start_app.current_user is not None:
-        return render_template('create-bucketlist.html')
+        if request.method == 'GET':
+            return render_template('create-bucketlist.html')
+        if request.method == 'POST':
+            bucket_list_name = request.form['bucket_list_name']
+            start_app.current_user.add_new_bucket_list(bucket_list_name)
+            return redirect(url_for('home'))
+    else:
+        return redirect(url_for('login'))
+
+
+@app.route('/viewbucketlist/<int:blist_id>', methods=['GET'])
+def viewbucketlist(blist_id):
+    start_app.current_bucketlist = blist_id
+    if start_app.current_user is not None:
+        if start_app.current_user.bucket_lists[blist_id].item_list:
+            return render_template('bucketlist-item.html',
+                                   users_buckets_items=start_app.current_user.bucket_lists[blist_id].item_list)
+        return render_template('bucketlist-item.html',
+                               users_buckets_items=start_app.current_user.bucket_lists[blist_id].item_list)
+    else:
+        return redirect(url_for('login'))
+
+
+@app.route('/create_bucketlist_item', methods=['GET', 'POST'])
+def create_bucketlist_item():
+    if start_app.current_user is not None:
+        if request.method == 'GET':
+            return render_template('create-bucketlist-item.html')
+        if request.method == 'POST':
+            item_name = request.form['name']
+            item_description = request.form['description']
+            start_app.current_user.bucket_lists[start_app.current_bucketlist].add_bucket_list_item(item_name, item_description)
+            return render_template('bucketlist-item.html',
+                                   users_buckets_items=start_app.current_user.bucket_lists[start_app.current_bucketlist].item_list)
     return redirect(url_for('login'))
