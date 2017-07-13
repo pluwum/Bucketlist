bcc78db174ecac78a790fa74f2151f1664be1e94
Brian Serumaga <bsk20111@hotmail.com>
2017-07-13 15:02:49 +0300

    Tweeked the classes

--- app/bucketlist/bucket_list_app.py
+++ app/bucketlist/bucket_list_app.py
@@ -6,6 +6,7 @@ class BucketListApp(object):
     """Class containing the bucket list methods"""
     def __init__(self):
         self.current_user = None
+        self.current_bucketlist = None
         self.list_of_users = []
 
     def add_user(self, fname, lname, email, password):
--- app/bucketlist/item.py
+++ app/bucketlist/item.py
@@ -3,7 +3,7 @@
 
 class Item(object):
     """Class used to store bucket list item properties"""
-    def __init__(self, name, description):
+    def __init__(self, name, description, bucket_list_id = None):
         self.name = name
         self.description = description
         self.progress = False
